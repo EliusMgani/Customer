d5877a87c5c03fcd1aac03d6142733d762047443
EliusMgani <eliasmgani@gmail.com>
2020-08-23 15:17:25 -0700

    little changes made

--- crm1/settings.py
+++ crm1/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lee22jar@gmail.com'
-EMAIL_HOST_PASSWORD = '123456789lee'
+EMAIL_HOST_USER = 'mycode778@gmail.com'
+EMAIL_HOST_PASSWORD = 'mycode@0425'
